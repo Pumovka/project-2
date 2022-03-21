ca738d0795470a9a236ee278f7791156069a4a5c
Polina Pumova <74357600+Pumovka@users.noreply.github.com>
2022-03-21 15:46:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-from msilib.schema import PublishComponent
-
-
-marks = {}
+ #создаю класс студентов, последнее поле пустое для суммы всех оценок 
 class Student:
-    def __init__(self, name, last_name, year, number_course, number_group, marks):
+    def __init__(self, name, last_name, year, number_course, number_group, marks, allmarks):
         self.name  = name
         self.last_name = last_name
         self.year = year 
         self.number_course = number_course 
         self.number_group = number_group 
         self.marks = marks
+        self.allmarks = allmarks
        #print(name, last_name, year, number_course, number_group, marks)
     pass
     def printStudent(self):
-        print(self.name, self.last_name, self.year, self.number_course, self.number_group, self.marks)
+        print(self.name, self.last_name, self.year, self.number_course, self.number_group, self.marks )
     pass
 
-student1 = Student("Василий", "Васильев", 1995, 3, "ВПИ-20",{"Математика": 5, "Русский": 3, "Химия": 3, "Экология": 3, "Экономика": 3  } )
-student2 = Student("Иван", "Иванов", 1998, 1, "ВПИ-21", {"Математика": 5, "Русский": 5, "Химия": 5, "Экология": 2, "Экономика": 2  } )
-student3 = Student("Петр", "Петров", 1994, 3, "ВПИ-20", {"Математика": 4, "Русский": 2, "Химия": 2, "Экология": 2, "Экономика": 2  } )
-student4 = Student("Егоров", "Егор", 1999, 1, "ВПИ-21", {"Математика": 5, "Русский": 5, "Химия": 5, "Экология": 5, "Экономика": 5  } )
-student5 = Student("Максимов", "Максим", 1997, 1, "ВПИ-21", {"Математика": 3, "Русский": 3, "Химия": 3, "Экология": 2, "Экономика": 2  } )
+student1 = Student("Василий", "Васильев", 1995, 3, "ВПИ-20",[5,5,5,5,5], 0 )
+student2 = Student("Иван", "Иванов", 1998, 1, "ВПИ-21", [5,4,4,5,5],  0 )
+student3 = Student("Петр", "Петров", 1994, 3, "ВПИ-20", [3,3,3,3,3] , 0 )
+student4 = Student("Егоров", "Егор", 1999, 1, "ВПИ-21", [5,3,4,3,2] , 0 )
+student5 = Student("Максимов", "Максим", 1997, 1, "ВПИ-21", [2,3,2,3,3] , 0 )
 list = [student1,student2,student3,student4,student5] 
 
 def printStuds(list):#вывод списка студентов
@@ -57,21 +55,61 @@ def min_year(list): #поиск самого младшего в списке
            name = list[i].name +" " +  list[i].last_name
     return min_, name
 
+
 list_VPI20 = []
 list_VPI21 = [] 
-# def Gpa(list):
-#     for i in list:
-#         if list[i].number_group == "ВПИ-20":
-#             list_VPI20.append(list[i])
-#         else:
-#              list_VPI21.append(list[i])
-# pass
-# Gpa(list)
+def separationGroup(list): #Разделение в списки на две группы
+    for i in range(len(list)): 
+        if list[i].number_group == "ВПИ-20":
+            list_VPI20.append(list[i])
+        else:
+             list_VPI21.append(list[i])
+pass
+separationGroup(list)
+# printStuds(list_VPI20) #вывод по группам
+# printStuds(list_VPI21)
+gpaMarksGroup =[]
+gpaMarksStuds =[]
+def GPA(list):
+    gpaMarksGroup =[0,0,0,0,0]
+    len_list = len(list)
+    for i in range(len_list):
+        for j in range(5):
+            gpaMarksGroup[j] = gpaMarksGroup[j] +list[i].marks[j] 
 
+    for i in range(len(gpaMarksGroup)):
+        print(i, "предмет:", gpaMarksGroup[i]/len_list)
+pass
+# printStuds(list_VPI20) #вывод по группам
+# printStuds(list_VPI21)
 
+def searchBest(list):
+    for i in range(len(list)):
+        for j in range(5):
+            list[i].allmarks = list[i].allmarks + list[i].marks[j]   
+            # print(list[i].allmarks) 
+    max_marks = list[0].allmarks
+    for i in range(len(list)):
+        if max_marks < list[i].allmarks:
+            print(list[i].name) #Не видит эту строку :( 
+            max_marks = list[i].allmarks
+         
+    return max_marks
 
-
-
+# printStuds(list_VPI20) #вывод по группам
+# printStuds(list_VPI21)
+#Вызов всех функций:
+print("ЗАДАНИЕ 1. Сортировка по курсу (от меньшего к большему)")
 funcSortCourse(list)
+print("ЗАДАНИЕ 2. Средний балл")
+print("Средний балл для первой группы по каждому предмету")
+GPA(list_VPI20)
+print("Средний балл для второй группы по каждому предмету")
+GPA(list_VPI21)
+print("ЗАДАНИЕ 3. Найти старшего и младшего")
 print("Самый старший", max_year(list))
 print("Самый младший", min_year(list))
+print("ЗАДАНИЕ 4. Поиск самого умного в группе")
+print("Самый большой рейтинг в ВПИ-20", searchBest(list_VPI20))
+print("Самый большой рейтинг в ВПИ-21", searchBest(list_VPI21))
+
